55e3fc4eb2ac887bd6c8699b3f32a6cbbe1dcbe3
Lee ChaeHyeon <chlee1001@naver.com>
2021-07-31 10:46:23 +0900

    21.07.31_init_springboot

---
+++ fin_demo/src/main/java/com/ch/fin_demo/FinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ch.fin_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinDemoApplication.class, args);
+    }
+
+}
